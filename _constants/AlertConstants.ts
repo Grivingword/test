8b993291f86c0c1d648f6f2d49c194e453cd8311
Grivingword <04.osvaldoarturo@gmail.com>
2022-11-22 22:47:49 -0600

    first commit

---
+++ _constants/AlertConstants.ts
@@ -0,0 +1,7 @@
+export const FAIl = 'Ocurrio un error inesperado';
+export const ERROR_DATA = 'Error al cargar datos';
+export const MESSAGE_ADD= 'Agregado con exito';
+export const MESSAGE_UPDATE= 'Actualizado con exito';
+export const MESSAGE_DELETE = 'Eliminado con exito';
+export const UNAUTHORIZED = 'Correo o Contraseña Incorrecto';
+export const WELCOME = 'Bienvenido a Veterinaria Cao';
---
+++ _constants/index.ts
@@ -0,0 +1,8 @@
+export { WELCOME } from './AlertConstants';
+export { UNAUTHORIZED } from './AlertConstants';
+export { THEME_DARK, THEME_LIGHT } from './ThemeConstants';
+export { ERROR_DATA } from './AlertConstants';
+export { FAIl } from './AlertConstants';
+export { MESSAGE_ADD } from './AlertConstants';
+export { MESSAGE_DELETE } from './AlertConstants';
+export { MESSAGE_UPDATE } from './AlertConstants';
